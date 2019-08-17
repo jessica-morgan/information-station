b9279905e5ed911b412a8672c31c2da88c748dbe
jess morgan <jess-morgan1@outlook.com>
2019-08-17 19:57:52 +1200

    combine reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,6 @@
-
-import {combineReducers} from 'redux'
-
-import example from './example'
+import { combineReducers } from 'redux'
+import selectedCategory from './category'
 
 export default combineReducers({
-  example
+  selectedCategory
 })
