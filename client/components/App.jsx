8ff1c3bffe2ca469e3962ac440f9060b24e61eb3
jess morgan <jess-morgan1@outlook.com>
2019-08-17 20:38:17 +1200

    App now stateful

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import ProgrammerHumorRedditFeed from './ProgrammerHumorRedditFeed'
-import RetconnedRedditFeed from './RetconnedRedditFeed'
-import IsItBullshitRedditFeed from './IsItBullshitRedditFeed'
-import TooAfraidToAskRedditFeed from './TooAfraidToAskFeed'
-import QuantumPhysicsFeed from './QuantumPhysicsFeed'
-import QuantumComputingFeed from './QuantumComputingFeed'
-import JavascriptFeed from './JavascriptFeed'
-import SpaceFeed from './SpaceFeed'
-import AstronomyPicOfTheDay from './AstronomyPicOfTheDay'
+import TitleList from './TitleList'
+import Welcome from './Welcome'
 import Nav from './Nav'
 
-const App = () => (
-  <div>
-    <div>
-      <Nav />
-    </div>
-    <Route path='/phFeed' component={ProgrammerHumorRedditFeed} />
-    <Route path='/rcFeed' component={RetconnedRedditFeed} />
-    <Route path='/bsFeed' component={IsItBullshitRedditFeed} />
-    <Route path='/tataFeed' component={TooAfraidToAskRedditFeed} />
-    <Route path='/qpFeed' component={QuantumPhysicsFeed} />
-    <Route path='/qcFeed' component={QuantumComputingFeed} />
-    <Route path='/jsFeed' component={JavascriptFeed} />
-    <Route path='/spaceFeed' component={SpaceFeed} />
-    <Route path='/apod' component={AstronomyPicOfTheDay} />
-  </div>
-)
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      categorySelected: false
+    }
+  }
+
+  render () {
+    return (
+      this.state.categorySelected
+        ? <div>
+          <Nav />
+          <div>
+            <TitleList />
+          </div>
+        </div>
+        : <div>
+          <Nav />
+          <div>
+            <Welcome />
+          </div>
+        </div>
+
+    )
+  }
+}
 
 export default App
