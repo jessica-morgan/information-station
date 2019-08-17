b8e30507eb0676ceeea39503f7f14aada5f6964a
jess morgan <jess-morgan1@outlook.com>
2019-08-17 13:29:08 +1200

    Retconned reddit posts feed created

---
+++ client/components/RetconnedRedditFeed.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { getRetconnedFeed } from '../redditApi'
+
+// this component should display a list of retconned posts (just the title)
+// create seperate componet for individual posts
+class RetconnedRedditFeed extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      retconnedPosts: []
+    }
+  }
+  componentDidMount () {
+    getRetconnedFeed()
+      .then(posts => {
+        this.setState({
+          retconnedPosts: posts
+        }
+        )
+      })
+  }
+  render () {
+    console.log(this.state.retconnedPosts)
+    return (
+      this.state.retconnedPosts.length
+        ? <div>
+          {this.state.retconnedPosts.map((post, idx) => {
+            return (
+              <div key={idx}>
+                <h1 key={post.title}>{post.title}</h1>
+                {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
+                {post.image.length ? <img key={post.image} src={post.image}></img> : <p></p>} */}
+              </div>
+            )
+          })}
+        </div> : <div>
+          {/* loading component */}
+        </div>
+    )
+  }
+}
+
+export default RetconnedRedditFeed
