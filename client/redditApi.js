fa85856ec801c67d81c40337bc9f31f9a6c5d07f
jess morgan <jess-morgan1@outlook.com>
2019-08-17 13:26:17 +1200

    added reddit feed apis

--- client/redditApi.js
+++ client/redditApi.js
@@ -2,27 +2,77 @@ import request from 'superagent'
 
 const programmerHumor = 'http://www.reddit.com/r/programmerHumor.json'
 
-function getProgrammerHumor () {
+const retconned = 'http://www.reddit.com/r/Retconned.json'
+
+const isItBullshit = 'http://www.reddit.com/r/IsItBullshit.json'
+
+const tooAfraidToAsk = 'http://www.reddit.com/r/TooAfraidToAsk.json'
+
+function getProgrammerHumorFeed () {
   return (
     request.get(`${programmerHumor}`)
       .then(posts => {
-        // console.log(posts.body.data.children)
-        let postsArray = posts.body.data.children
-        let postTitle = postsArray.map(post => post.data.title)
-        let postBody = postsArray.map(post => post.data.selftext)
-        let postImage = postsArray.map(post => post.data.url)
-        // splice utc format to add . and add E+12 on the end
-        // let postCreated = postsArray.map(post => post.created_utc)
-        // let formatedCreatedDate = new Date(postCreated).toLocaleString()
-        let postData = {
-          title: postTitle,
-          body: postBody,
-          image: postImage
-          // created: formatedCreatedDate
-        }
-        return postData
+        let newPHPostsArray = []
+        posts.body.data.children.map(post => {
+          newPHPostsArray.push({ title: post.data.title, body: post.data.selftext, image: post.data.url })
+          return newPHPostsArray
+        })
+        return newPHPostsArray
       })
   )
 }
 
-module.exports = { getProgrammerHumor }
+function getRetconnedFeed () {
+  return (
+    request.get(`${retconned}`)
+      .then(
+        posts => {
+          let newRetPostsArray = []
+          posts.body.data.children.map(post => {
+            newRetPostsArray.push({ title: post.data.title, body: post.data.selftext, image: post.data.url })
+            return newRetPostsArray
+          })
+          return newRetPostsArray
+        }
+      )
+  )
+}
+
+function getIsItBullshitFeed () {
+  return (
+    request.get(`${isItBullshit}`)
+      .then(
+        posts => {
+          let newBSPostsArray = []
+          posts.body.data.children.map(post => {
+            newBSPostsArray.push({ title: post.data.title, body: post.data.selftext, image: post.data.url })
+            return newBSPostsArray
+          })
+          return newBSPostsArray
+        }
+      )
+  )
+}
+
+function getTooAfraidFeed () {
+  return (
+    request.get(`${tooAfraidToAsk}`)
+      .then(
+        posts => {
+          let newTATAPostsArray = []
+          posts.body.data.children.map(post => {
+            newTATAPostsArray.push({ title: post.data.title, body: post.data.selftext, image: post.data.url })
+            return newTATAPostsArray
+          })
+          return newTATAPostsArray
+        }
+      )
+  )
+}
+
+module.exports = {
+  getProgrammerHumorFeed,
+  getRetconnedFeed,
+  getIsItBullshitFeed,
+  getTooAfraidFeed
+}
