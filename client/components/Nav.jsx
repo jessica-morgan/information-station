21996b2767dd8fb1305162910337ede5ff047b30
jess morgan <jess-morgan1@outlook.com>
2019-08-17 23:27:22 +1200

    nav statefull

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { categoryAction } from '../actions/category'
 
-const Nav = () => (
-  <div>
-    <h2>Reddit</h2>
-    <Link to='/phFeed'><h3>Programmer Humor</h3></Link>
-    <Link to='/rcFeed'><h3>Retconned</h3></Link>
-    <Link to='/bsFeed'><h3>Is It Bullshit</h3></Link>
-    <Link to='tataFeed'><h3>Too Afraid To Ask</h3></Link>
-    <br/>
-    <h2>News</h2>
-    <Link to='/jsFeed'><h3>Javascript</h3></Link>
-    <Link to='/qcFeed'><h3>Quantum Computing</h3></Link>
-    <Link to='/qpFeed'><h3>Quantum Physics</h3></Link>
-    <Link to='spaceFeed'><h3>Space</h3></Link>
-    <br/>
-    <Link to='/apod'><h2>Astronomy Picture of the Day</h2></Link>
-  </div>
-)
+class Nav extends React.Component {
+  render () {
+    return (
+      <div style={{ display: 'block', width: '20vw', height: '100%', float: 'left', position: 'fixed' }}>
+        <h2>Reddit</h2>
+        <Link to='/phFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Programmer Humor</h3></Link>
+        <Link to='/rcFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Retconned</h3></Link>
+        <Link to='/bsFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Is It Bullshit</h3></Link>
+        <Link to='tataFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Too Afraid To Ask</h3></Link>
+        <br/>
+        <h2>News</h2>
+        <Link to='/jsFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Javascript</h3></Link>
+        <Link to='/qcFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Quantum Computing</h3></Link>
+        <Link to='/qpFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Quantum Physics</h3></Link>
+        <Link to='spaceFeed' onClick={() => this.props.dispatch(categoryAction(true))}><h3>Space</h3></Link>
+        <br/>
+        <Link to='/apod' onClick={() => this.props.dispatch(categoryAction(true))}><h2>Astronomy Picture of the Day</h2></Link>
+      </div>
+    )
+  }
+}
 
-export default Nav
+function mapStateToProps (state) {
+  return {
+    isCategorySelected: state.categorySelected.categoryState
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
