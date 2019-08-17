32879d8cda0efa3629a7984d76e30eaf26c445f0
jess morgan <jess-morgan1@outlook.com>
2019-08-17 13:29:26 +1200

    TooAfraidToAsk reddit posts feed

---
+++ client/components/TooAfraidToAskFeed.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { getTooAfraidFeed } from '../redditApi'
+
+// this component should display a list of tata posts (just the title)
+// create seperate componet for individual posts
+class TooAfraidToAskRedditFeed extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      tataPosts: []
+    }
+  }
+  componentDidMount () {
+    getTooAfraidFeed()
+      .then(posts => {
+        this.setState({
+          tataPosts: posts
+        }
+        )
+      })
+  }
+  render () {
+    return (
+      this.state.tataPosts.length
+        ? <div>
+          {this.state.tataPosts.map((post, idx) => {
+            return (
+              <div key={idx}>
+                <h1 key={post.title}>{post.title}</h1>
+                {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
+                {post.image.length ? <img key={post.image} src={post.image}></img> : <p></p>} */}
+              </div>
+            )
+          })}
+        </div> : <div>
+          {/* loading component */}
+        </div>
+    )
+  }
+}
+
+export default TooAfraidToAskRedditFeed
