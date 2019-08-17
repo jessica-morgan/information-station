de817f118f1d0c3205a9cf2d62e8bf93dbb4507a
jess morgan <jess-morgan1@outlook.com>
2019-08-17 13:28:07 +1200

    routes to post feeds

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import RedditFeed from './RedditFeed'
+import ProgrammerHumorRedditFeed from './ProgrammerHumorRedditFeed'
+import RetconnedRedditFeed from './RetconnedRedditFeed'
+import IsItBullshitRedditFeed from './IsItBullshitRedditFeed'
+import TooAfraidToAskRedditFeed from './TooAfraidToAskFeed'
 
 const App = () => (
   <div>
-    <Route path='/' component={RedditFeed} />
+    <Route path='/phFeed' component={ProgrammerHumorRedditFeed} />
+    <Route path='/rcFeed' component={RetconnedRedditFeed} />
+    <Route path='/bsFeed' component={IsItBullshitRedditFeed} />
+    <Route path='/tataFeed' component={TooAfraidToAskRedditFeed} />
   </div>
 )
 
