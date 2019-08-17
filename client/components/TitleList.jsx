39d9895ffeb7edb9c13cf00627f0e7945792c1cf
jess morgan <jess-morgan1@outlook.com>
2019-08-17 20:38:30 +1200

    imported components

--- client/components/TitleList.jsx
+++ client/components/TitleList.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import ProgrammerHumorRedditFeed from './ProgrammerHumorRedditFeed'
+import RetconnedRedditFeed from './RetconnedRedditFeed'
+import IsItBullshitRedditFeed from './IsItBullshitRedditFeed'
+import TooAfraidToAskRedditFeed from './TooAfraidToAskFeed'
+import QuantumPhysicsFeed from './QuantumPhysicsFeed'
+import QuantumComputingFeed from './QuantumComputingFeed'
+import JavascriptFeed from './JavascriptFeed'
+import SpaceFeed from './SpaceFeed'
+import AstronomyPicOfTheDay from './AstronomyPicOfTheDay'
 
 class TitleList extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      
+
     }
   }
 
   render () {
+    const category = this.props.categorySelected
     return (
-      <div>
-
-      </div>
+      
     )
   }
 }
 
-export default TitleList
+function mapStateToProps (state) {
+  return {
+    categorySelected: state.selectedCategory.categoryState
+  }
+}
+
+export default connect(mapStateToProps)(TitleList)
