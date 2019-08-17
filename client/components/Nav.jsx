95bc43f403de111ecc35402e44a8c925adb50206
jess morgan <jess-morgan1@outlook.com>
2019-08-17 19:27:29 +1200

    made nav class component

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 // nav component shows a list of available options:
 // reddit: list subreddits
 // news: list categories
 // astronomy pic of the day
-const Nav = () => (
-  <div>
-    <h2>Reddit</h2>
-    <Link to='/phFeed'><h3>Programmer Humor</h3></Link>
-    <Link to='/rcFeed'><h3>Retconned</h3></Link>
-    <Link to='/bsFeed'><h3>Is It Bullshit</h3></Link>
-    <Link to='tataFeed'><h3>Too Afraid To Ask</h3></Link>
-    <br/>
-    <h2>News</h2>
-    <Link to='/jsFeed'><h3>Javascript</h3></Link>
-    <Link to='/qcFeed'><h3>Quantum Computing</h3></Link>
-    <Link to='/qpFeed'><h3>Quantum Physics</h3></Link>
-    <Link to='spaceFeed'><h3>Space</h3></Link>
-    <br/>
-    <Link to='/apod'><h2>Astronomy Picture of the Day</h2></Link>
-  </div>
-)
+class Nav extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selectedCategory: ''
+    }
+  }
+
+  render () {
+    return (
+      <div style={{ display: 'block', width: '20vw', height: '100%', float: 'left', position: 'fixed' }}>
+        <h2>Reddit</h2>
+        <Link to='/phFeed'><h3>Programmer Humor</h3></Link>
+        <Link to='/rcFeed'><h3>Retconned</h3></Link>
+        <Link to='/bsFeed'><h3>Is It Bullshit</h3></Link>
+        <Link to='tataFeed'><h3>Too Afraid To Ask</h3></Link>
+        <br/>
+        <h2>News</h2>
+        <Link to='/jsFeed'><h3>Javascript</h3></Link>
+        <Link to='/qcFeed'><h3>Quantum Computing</h3></Link>
+        <Link to='/qpFeed'><h3>Quantum Physics</h3></Link>
+        <Link to='spaceFeed'><h3>Space</h3></Link>
+        <br/>
+        <Link to='/apod'><h2>Astronomy Picture of the Day</h2></Link>
+      </div>
+    )
+  }
+}
 
 export default Nav
