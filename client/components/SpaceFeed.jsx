39cbe176a2f18c3105f70e0a3cc4507fa8f5c48c
jess morgan <jess-morgan1@outlook.com>
2019-08-19 13:48:37 +1200

    implemented hooks

--- client/components/SpaceFeed.jsx
+++ client/components/SpaceFeed.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getSpaceNewsFeed } from '../api/newsApi'
 
 // this component should display a list of space (just the title)
 // create seperate componet for individual articles
-class SpaceFeed extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      spaceNews: []
-    }
-  }
-  componentDidMount () {
+const SpaceFeed = () => {
+  const [spaceFeed, setSpaceFeed] = useState()
+
+  useEffect(() => {
     getSpaceNewsFeed()
       .then(articles => {
-        this.setState({
+        setSpaceFeed({
           spaceNews: articles
-        }
-        )
+        })
       })
-  }
-  render () {
-    return (
-      this.state.spaceNews.length
-        ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {this.state.spaceNews.map((article, idx) => {
-            return (
-              <div key={idx}>
-                <h1 key={article.title}>{article.title}</h1>
-              </div>
-            )
-          })}
-        </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+  }, [])
+
+  return spaceFeed ? (
+    <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {spaceFeed.spaceNews.map((article, idx) => {
+        return (
+          <div key={idx}>
+            <h1 key={article.title}>{article.title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
+    : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
         loading component
-        </div>
-    )
-  }
+    </div>
 }
 
 export default SpaceFeed
--- client/components/TooAfraidToAskFeed.jsx
+++ client/components/TooAfraidToAskFeed.jsx
@@ -1,43 +1,36 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import { getTooAfraidFeed } from '../api/redditApi'
 
 // this component should display a list of tata posts (just the title)
 // create seperate componet for individual posts
-class TooAfraidToAskRedditFeed extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      tataPosts: []
-    }
-  }
-  componentDidMount () {
+const TooAfraidToAskRedditFeed = () => {
+  const [tataFeed, setTataFeed] = useState()
+
+  useEffect(() => {
     getTooAfraidFeed()
       .then(posts => {
-        this.setState({
+        setTataFeed({
           tataPosts: posts
-        }
-        )
+        })
       })
-  }
-  render () {
-    return (
-      this.state.tataPosts.length
-        ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {this.state.tataPosts.map((post, idx) => {
-            return (
-              <div key={idx}>
-                <h1 key={post.title}>{post.title}</h1>
-                {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
+  }, [])
+
+  return tataFeed ? (
+    <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {tataFeed.tataPosts.map((post, idx) => {
+        return (
+          <div key={idx}>
+            <h1 key={post.title}>{post.title}</h1>
+            {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
                 {post.image.length ? <img key={post.image} src={post.image}></img> : <p></p>} */}
-              </div>
-            )
-          })}
-        </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {/* loading component */}
-        </div>
-    )
-  }
+          </div>
+        )
+      })}
+    </div>
+  )
+    : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {/* loading component */}
+    </div>
 }
 
 export default TooAfraidToAskRedditFeed
