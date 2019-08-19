24e8e27b637c8f93f3b416aebc2dc9240ab044af
jess morgan <jess-morgan1@outlook.com>
2019-08-19 13:03:27 +1200

    implemented hooks

--- client/components/RetconnedRedditFeed.jsx
+++ client/components/RetconnedRedditFeed.jsx
@@ -1,44 +1,36 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import { getRetconnedFeed } from '../api/redditApi'
 
 // this component should display a list of retconned posts (just the title)
 // create seperate componet for individual posts
-class RetconnedRedditFeed extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      retconnedPosts: []
-    }
-  }
-  componentDidMount () {
+const RetconnedRedditFeed = () => {
+  const [retconnedFeed, setRetconnedFeed] = useState()
+
+  useEffect(() => {
     getRetconnedFeed()
       .then(posts => {
-        this.setState({
+        setRetconnedFeed({
           retconnedPosts: posts
-        }
-        )
+        })
       })
-  }
-  render () {
-    console.log(this.state.retconnedPosts)
-    return (
-      this.state.retconnedPosts.length
-        ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {this.state.retconnedPosts.map((post, idx) => {
-            return (
-              <div key={idx}>
-                <h1 key={post.title}>{post.title}</h1>
-                {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
+  }, [])
+
+  return retconnedFeed ? (
+    <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {retconnedFeed.retconnedPosts.map((post, idx) => {
+        return (
+          <div key={idx}>
+            <h1 key={post.title}>{post.title}</h1>
+            {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
                 {post.image.length ? <img key={post.image} src={post.image}></img> : <p></p>} */}
-              </div>
-            )
-          })}
-        </div > : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {/* loading component */}
-        </div>
-    )
-  }
+          </div>
+        )
+      })}
+    </div >
+  )
+    : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {/* loading component */}
+    </div>
 }
 
 export default RetconnedRedditFeed
