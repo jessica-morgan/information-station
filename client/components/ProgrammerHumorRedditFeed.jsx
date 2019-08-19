26bb73f86c01cfd87db9256611c29bfffeb29442
jess morgan <jess-morgan1@outlook.com>
2019-08-19 12:46:58 +1200

    implemented hooks

--- client/components/ProgrammerHumorRedditFeed.jsx
+++ client/components/ProgrammerHumorRedditFeed.jsx
@@ -1,44 +1,34 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import { getProgrammerHumorFeed } from '../api/redditApi'
 
 // this component should display a list of programmerHumor posts (just the title)
 // create seperate componet for individual posts
-class ProgrammerHumorRedditFeed extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      programmerHumorPosts: []
-    }
-  }
-  componentDidMount () {
+const ProgrammerHumorRedditFeed = () => {
+  const [programmerHumorFeed, setProgrammerHumorFeed] = useState()
+
+  useEffect(() => {
     getProgrammerHumorFeed()
       .then(posts => {
-        this.setState({
+        setProgrammerHumorFeed({
           programmerHumorPosts: posts
-        }
-        )
+        })
       })
-  }
-  render () {
-    console.log(this.state.programmerHumorPosts)
-    return (
-      this.state.programmerHumorPosts.length
-        ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {this.state.programmerHumorPosts.map((post, idx) => {
-            return (
-              <div key={idx}>
-                <h1 key={post.title}>{post.title}</h1>
-                {/* {post.body.length ? <p key={post.body}>{post.body}</p> : <p></p>}
-                {post.image.length ? <img key={post.image} src={post.image}></img> : <p></p>} */}
-              </div>
-            )
-          })}
-        </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {/* loading component */}
-        </div>
-    )
-  }
+  }, [])
+
+  return programmerHumorFeed ? (
+    <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {programmerHumorFeed.programmerHumorPosts.map((post, idx) => {
+        return (
+          <div key={idx}>
+            <h1 key={post.title}>{post.title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
+    : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+       loading component
+    </div>
 }
 
 export default ProgrammerHumorRedditFeed
