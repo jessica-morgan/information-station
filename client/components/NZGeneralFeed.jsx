2ef4b072934c52fe1f9893ed717401576c4e4be1
jess morgan <jess-morgan1@outlook.com>
2019-08-19 12:39:17 +1200

    implemented hooks

--- client/components/NZGeneralFeed.jsx
+++ client/components/NZGeneralFeed.jsx
@@ -1,38 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getNZGeneralHeadlines } from '../api/newsApi'
 
-class NZGeneralFeed extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      nzHeadlines: []
-    }
-  }
-  componentDidMount () {
+const NZGeneralFeed = () => {
+  const [nzGeneralHeadlines, setNzGeneralHeadlines] = useState()
+
+  useEffect(() => {
     getNZGeneralHeadlines()
-      .then(headlines => {
-        this.setState({
-          nzHeadlines: headlines
-        }
-        )
+      .then(articles => {
+        setNzGeneralHeadlines({
+          headlines: articles
+        })
       })
-  }
-  render () {
-    return (
-      this.state.nzHeadlines.length
-        ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {this.state.nzHeadlines.map((headline, idx) => {
-            return (
-              <div key={idx}>
-                <h1 key={headline.title}>{headline.title}</h1>
-              </div>
-            )
-          })}
-        </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+  }, [])
+
+  return nzGeneralHeadlines ? (
+    <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {nzGeneralHeadlines.headlines.map((headline, idx) => {
+        return (
+          <div key={idx}>
+            <h1 key={headline.title}>{headline.title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
+    : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
         loading component
-        </div>
-    )
-  }
+    </div>
 }
+
+// class NZGeneralFeed extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       nzHeadlines: []
+//     }
+//   }
+//   componentDidMount () {
+//     getNZGeneralHeadlines()
+//       .then(headlines => {
+//         this.setState({
+//           nzHeadlines: headlines
+//         }
+//         )
+//       })
+//   }
+//   render () {
+//     return (
+//       this.state.nzHeadlines.length
+//         ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+//           {this.state.nzHeadlines.map((headline, idx) => {
+//             return (
+//               <div key={idx}>
+//                 <h1 key={headline.title}>{headline.title}</h1>
+//               </div>
+//             )
+//           })}
+//         </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+//         loading component
+//         </div>
+//     )
+//   }
+// }
 
 export default NZGeneralFeed
--- public/bundle.js
+++ public/bundle.js
@@ -45992,7 +45992,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 var _react = __webpack_require__(2);
 
@@ -46002,64 +46002,74 @@ var _newsApi = __webpack_require__(7);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var NZGeneralFeed = function NZGeneralFeed() {
+  var _useState = (0, _react.useState)(),
+      _useState2 = _slicedToArray(_useState, 2),
+      nzGeneralHeadlines = _useState2[0],
+      setNzGeneralHeadlines = _useState2[1];
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var NZGeneralFeed = function (_React$Component) {
-  _inherits(NZGeneralFeed, _React$Component);
-
-  function NZGeneralFeed(props) {
-    _classCallCheck(this, NZGeneralFeed);
-
-    var _this = _possibleConstructorReturn(this, (NZGeneralFeed.__proto__ || Object.getPrototypeOf(NZGeneralFeed)).call(this, props));
-
-    _this.state = {
-      nzHeadlines: []
-    };
-    return _this;
-  }
-
-  _createClass(NZGeneralFeed, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      var _this2 = this;
-
-      (0, _newsApi.getNZGeneralHeadlines)().then(function (headlines) {
-        _this2.setState({
-          nzHeadlines: headlines
-        });
+  (0, _react.useEffect)(function () {
+    (0, _newsApi.getNZGeneralHeadlines)().then(function (articles) {
+      setNzGeneralHeadlines({
+        headlines: articles
       });
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      return this.state.nzHeadlines.length ? _react2.default.createElement(
-        'div',
-        { style: { display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' } },
-        this.state.nzHeadlines.map(function (headline, idx) {
-          return _react2.default.createElement(
-            'div',
-            { key: idx },
-            _react2.default.createElement(
-              'h1',
-              { key: headline.title },
-              headline.title
-            )
-          );
-        })
-      ) : _react2.default.createElement(
-        'div',
-        { style: { display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' } },
-        'loading component'
-      );
-    }
-  }]);
+    });
+  }, []);
 
-  return NZGeneralFeed;
-}(_react2.default.Component);
+  return nzGeneralHeadlines ? _react2.default.createElement(
+    'div',
+    { style: { display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' } },
+    nzGeneralHeadlines.headlines.map(function (headline, idx) {
+      return _react2.default.createElement(
+        'div',
+        { key: idx },
+        _react2.default.createElement(
+          'h1',
+          { key: headline.title },
+          headline.title
+        )
+      );
+    })
+  ) : _react2.default.createElement(
+    'div',
+    { style: { display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' } },
+    'loading component'
+  );
+};
+
+// class NZGeneralFeed extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       nzHeadlines: []
+//     }
+//   }
+//   componentDidMount () {
+//     getNZGeneralHeadlines()
+//       .then(headlines => {
+//         this.setState({
+//           nzHeadlines: headlines
+//         }
+//         )
+//       })
+//   }
+//   render () {
+//     return (
+//       this.state.nzHeadlines.length
+//         ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+//           {this.state.nzHeadlines.map((headline, idx) => {
+//             return (
+//               <div key={idx}>
+//                 <h1 key={headline.title}>{headline.title}</h1>
+//               </div>
+//             )
+//           })}
+//         </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+//         loading component
+//         </div>
+//     )
+//   }
+// }
 
 exports.default = NZGeneralFeed;
 
