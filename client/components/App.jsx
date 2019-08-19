974d440477a15b233d1c9610c5d75ed381550b88
jess morgan <jess-morgan1@outlook.com>
2019-08-19 13:49:04 +1200

    app now using global state w reactn

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import { useGlobal } from 'reactn'
 import { Route } from 'react-router-dom'
-import { connect } from 'react-redux'
 import Welcome from './Welcome'
 import Nav from './Nav'
 import ProgrammerHumorRedditFeed from './ProgrammerHumorRedditFeed'
@@ -15,39 +15,30 @@ import AstronomyPicOfTheDay from './AstronomyPicOfTheDay'
 import NZScienceTechnologyFeed from './NZScienceTechnologyFeed'
 import NZGeneralFeed from './NZGeneralFeed'
 
-class App extends React.Component {
-  render () {
-    return (
+const App = () => {
+  const [isCategorySelected, setIsCategorySelected] = useGlobal()
+  console.log(isCategorySelected)
+  return isCategorySelected ? (
+    <div>
+      <Nav />
       <div>
-        <Nav />
-        {this.props.isCategorySelected
-          ? <div>
-            <Route path='/phFeed' component={ProgrammerHumorRedditFeed} />
-            <Route path='/rcFeed' component={RetconnedRedditFeed} />
-            <Route path='/bsFeed' component={IsItBullshitRedditFeed} />
-            <Route path='/tataFeed' component={TooAfraidToAskRedditFeed} />
-            <Route path='/qpFeed' component={QuantumPhysicsFeed} />
-            <Route path='/qcFeed' component={QuantumComputingFeed} />
-            <Route path='/jsFeed' component={JavascriptFeed} />
-            <Route path='/spaceFeed' component={SpaceFeed} />
-            <Route path='/apod' component={AstronomyPicOfTheDay} />
-            <Route path='/nzstFeed' component={NZScienceTechnologyFeed} />
-            <Route path='/nzgFeed' component={NZGeneralFeed} />
-
-          </div>
-          : <div>
-            <Welcome />
-          </div>
-        }
+        <Route path='/phFeed' component={ProgrammerHumorRedditFeed} />
+        <Route path='/rcFeed' component={RetconnedRedditFeed} />
+        <Route path='/bsFeed' component={IsItBullshitRedditFeed} />
+        <Route path='/tataFeed' component={TooAfraidToAskRedditFeed} />
+        <Route path='/qpFeed' component={QuantumPhysicsFeed} />
+        <Route path='/qcFeed' component={QuantumComputingFeed} />
+        <Route path='/jsFeed' component={JavascriptFeed} />
+        <Route path='/spaceFeed' component={SpaceFeed} />
+        <Route path='/apod' component={AstronomyPicOfTheDay} />
+        <Route path='/nzstFeed' component={NZScienceTechnologyFeed} />
+        <Route path='/nzgFeed' component={NZGeneralFeed} />
       </div>
-    )
-  }
+    </div>
+  )
+    : <div>
+      <Welcome />
+    </div>
 }
 
-function mapStateToProps (state) {
-  return {
-    isCategorySelected: state.categorySelected.categoryState
-  }
-}
-
-export default connect(mapStateToProps)(App)
+export default App
