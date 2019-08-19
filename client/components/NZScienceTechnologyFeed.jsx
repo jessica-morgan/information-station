68ef7630dcb66b2b544f812183e3c9c5ddb8c2e5
jess morgan <jess-morgan1@outlook.com>
2019-08-19 12:43:54 +1200

    implemented hooks

--- client/components/NZScienceTechnologyFeed.jsx
+++ client/components/NZScienceTechnologyFeed.jsx
@@ -1,38 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getNZScienceTechnologyHeadlines } from '../api/newsApi'
 
-class NZScienceTechnologyFeed extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      nzHeadlines: []
-    }
-  }
-  componentDidMount () {
+// this component should display a list of nzst headlines (just the title)
+// create seperate componet for individual articles
+const NZScienceTechnologyFeed = () => {
+  const [nzScienceTechnologyHeadlines, setNzScienceTechnologyHeadlines] = useState()
+
+  useEffect(() => {
     getNZScienceTechnologyHeadlines()
-      .then(headlines => {
-        this.setState({
-          nzHeadlines: headlines
-        }
-        )
+      .then(articles => {
+        setNzScienceTechnologyHeadlines({
+          headlines: articles
+        })
       })
-  }
-  render () {
-    return (
-      this.state.nzHeadlines.length
-        ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-          {this.state.nzHeadlines.map((headline, idx) => {
-            return (
-              <div key={idx}>
-                <h1 key={headline.title}>{headline.title}</h1>
-              </div>
-            )
-          })}
-        </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+  }, [])
+  return nzScienceTechnologyHeadlines ? (
+    <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
+      {nzScienceTechnologyHeadlines.headlines.map((headline, idx) => {
+        return (
+          <div key={idx}>
+            <h1 key={headline.title}>{headline.title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
+    : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
         loading component
-        </div>
-    )
-  }
+    </div>
 }
 
 export default NZScienceTechnologyFeed
