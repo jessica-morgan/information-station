aac50a8cf51258bb943f5bb60ee42ddba06de296
jess morgan <jess-morgan1@outlook.com>
2019-08-19 12:43:59 +1200

    comments

--- client/components/NZGeneralFeed.jsx
+++ client/components/NZGeneralFeed.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { getNZGeneralHeadlines } from '../api/newsApi'
 
+// this component should display a list of nzg headlines (just the title)
+// create seperate componet for individual articles
 const NZGeneralFeed = () => {
   const [nzGeneralHeadlines, setNzGeneralHeadlines] = useState()
 
@@ -28,39 +30,5 @@ const NZGeneralFeed = () => {
         loading component
     </div>
 }
-
-// class NZGeneralFeed extends React.Component {
-//   constructor (props) {
-//     super(props)
-//     this.state = {
-//       nzHeadlines: []
-//     }
-//   }
-//   componentDidMount () {
-//     getNZGeneralHeadlines()
-//       .then(headlines => {
-//         this.setState({
-//           nzHeadlines: headlines
-//         }
-//         )
-//       })
-//   }
-//   render () {
-//     return (
-//       this.state.nzHeadlines.length
-//         ? <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-//           {this.state.nzHeadlines.map((headline, idx) => {
-//             return (
-//               <div key={idx}>
-//                 <h1 key={headline.title}>{headline.title}</h1>
-//               </div>
-//             )
-//           })}
-//         </div> : <div style={{ display: 'block', width: '70vw', height: '100%', float: 'right', position: 'relative' }}>
-//         loading component
-//         </div>
-//     )
-//   }
-// }
 
 export default NZGeneralFeed
