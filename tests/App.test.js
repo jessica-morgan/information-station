b536f2bf9a41640362ec7c35741986c924f25b42
jess morgan <jess-morgan1@outlook.com>
2019-09-04 21:02:30 +1200

    test files

--- tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow, render, mount } from 'enzyme'
-
-import App from '../client/components/App'
-
-test('test runner is working', () => {
-  expect(true).toBeTruthy()
-})
-
---
+++ tests/IsItBullshit.test.js
@@ -0,0 +1,6 @@
+// test that selectTitle gets called with on click
+// test that currentTitleSelected gets called with on click
+// test that use global fns are called with correct properties
+// figure out how to test useeffect and that it calls setPosts?
+// test that setPosts is called with allPosts: array
+// test that the expected number of h1s are rendered (check api fetch limit for expected number)
