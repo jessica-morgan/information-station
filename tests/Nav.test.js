c88c06ea1d9631fb5f8bc00fdaecd1640f99ef7d
jess morgan <jess-morgan1@outlook.com>
2019-09-12 00:10:37 +1200

    Nav component tests

--- tests/Nav.test.js
+++ tests/Nav.test.js
@@ -1,17 +1,20 @@
-// test for links
+import React from 'reactn'
+import Nav from '../client/components/Nav'
+import renderer from 'react-test-renderer'
+import { MemoryRouter } from 'react-router-dom'
+import { shallow } from 'enzyme'
 
-// test('<TopMenu /> contains a link', () => {
-//   // expect 'Add Food'
-//   const expected = 'Add Food'
-//   // assign wrapper to render TopMenu component wrapped in memoryrouter
-//   const wrapper = render(<MemoryRouter><TopMenu/></MemoryRouter>)
-//   // assign actual to wrappers text
-//   const actual = wrapper.text()
-//   // expect TupMenu text to match 'Add Food'
-//   expect(actual).toMatch(expected)
-// })
+test('Nav renders correctly', () => {
+  const component = renderer.create(
+    <MemoryRouter>
+      <Nav />
+    </MemoryRouter>
+  )
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
-// Returns a string of the rendered text of the current render tree. This function should be looked at
-// with skepticism if being used to test what the actual HTML output of the component will be. If that
-// is what you would like to test, use enzyme's render function instead.
-// Note: can only be called on a wrapper of a single node.
+test('Nav contains this text', () => {
+  const wrapper = shallow(<Nav />)
+  expect(wrapper.text()).toMatch('New Zealand' && 'Reddit' && 'News')
+})
