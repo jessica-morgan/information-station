519385c4574d8eda53f1765864b0d2bac4546c18
jess morgan <jess-morgan1@outlook.com>
2019-09-14 15:34:30 +1200

    App test

--- tests/ComponentTests/App.test.js
+++ tests/ComponentTests/App.test.js
@@ -1,1 +1,8 @@
-// tests
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from '../../client/components/App'
+
+test('<App /> contains <Nav/> and <Welcome/>', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.text()).toBe('<Nav /><Welcome />')
+})
