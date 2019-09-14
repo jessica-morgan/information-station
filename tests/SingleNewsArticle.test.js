72c8a1156c119478b379af160552bc2f69cc80d7
jess morgan <jess-morgan1@outlook.com>
2019-09-14 14:28:45 +1200

    test for SingleNewsArticle

--- tests/SingleNewsArticle.test.js
+++ tests/SingleNewsArticle.test.js
@@ -1,2 +1,58 @@
-// tests
+import React from 'reactn'
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
+import SingleNewsArticle from '../client/components/SingleRedditPost'
+const newsApi = require('../client/api/newsApi')
 
+test('<SingleNewsArticle /> recieves correct data as props', () => {
+  return newsApi.getQuatumPhysicsNewsFeed().then(response => {
+    const wrapper = shallow(
+      <MemoryRouter>
+        <SingleNewsArticle posts={response[0]}/>
+      </MemoryRouter>
+    )
+    expect(wrapper.children().props().posts).toEqual(expect.objectContaining({
+      author: expect.anything() || null,
+      content: expect.anything() || null,
+      title: expect.anything() || null,
+      description: expect.anything() || null,
+      url: expect.anything() || null,
+      urlToImage: expect.anything() || null,
+      publishedAt: expect.anything() || null
+    })
+    )
+  })
+})
+
+test('<SingleNewsArticle/> contains correct tags when passed props', () => {
+  return newsApi.getQuatumPhysicsNewsFeed().then(response => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <SingleNewsArticle posts={response}/>
+      </MemoryRouter>
+    )
+    expect(wrapper.containsAllMatchingElements([
+      <div key={'title'}>
+        <h1></h1>
+        <br />
+        <h3 ></h3>
+        <br />
+        <h3></h3>
+        <br />
+        <img/>
+        <br />
+        <h2></h2>
+      </div>
+    ]))
+  })
+})
+
+test('<SingleNewsArticle/> renders empty div when no props recieved', () => {
+  const wrapper = shallow(
+    <MemoryRouter>
+      <SingleNewsArticle/>
+    </MemoryRouter>)
+  expect(wrapper.containsAllMatchingElements([
+    <div></div>
+  ]))
+})
