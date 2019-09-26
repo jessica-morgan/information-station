6e04e174ea6c17ecdc0f367e9be12a7f99880975
jess morgan <jess-morgan1@outlook.com>
2019-09-26 16:01:17 +1200

    nav link styling

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'reactn'
 import { selectCategory } from '../utils'
 import { Link } from 'react-router-dom'
-import { H3, NavLi, NavUl } from '../styles'
+import { H3, NavLi, NavUl, NavCategories } from '../styles'
 import CurrentCategoryTitle from './CurrentCategoryTitle'
 
 class Nav extends React.Component {
@@ -34,34 +34,34 @@ class Nav extends React.Component {
       <CurrentCategoryTitle/>
       <NavUl>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/apod' onClick={() => selectCategory('Nasa Picture of the Day') }><H3>NASA PICTURE OF THE DAY</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/apod' onClick={() => selectCategory('Nasa Picture of the Day') }><NavCategories>NASA PICTURE OF THE DAY</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/phFeed' onClick={() => selectCategory('Programmer Humor')}><H3>PROGRAMMER HUMOR</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/phFeed' onClick={() => selectCategory('Programmer Humor')}><NavCategories>PROGRAMMER HUMOR</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/rjsFeed' onClick={() => selectCategory('Reactjs')}><H3>REACTJS</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/rjsFeed' onClick={() => selectCategory('Reactjs')}><NavCategories>REACTJS</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/cgpFeed' onClick={() => selectCategory('Cool Github Projects')}><H3>COOL GITHUB PROJECTS</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/cgpFeed' onClick={() => selectCategory('Cool Github Projects')}><NavCategories>COOL GITHUB PROJECTS</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='jsFeed' onClick={() => selectCategory('Javascript')}><H3>JAVASCRIPT</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='jsFeed' onClick={() => selectCategory('Javascript')}><NavCategories>JAVASCRIPT</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/nzgFeed' onClick={() => selectCategory('General NZ')}><H3>GENERAL NZ</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/nzgFeed' onClick={() => selectCategory('General NZ')}><NavCategories>GENERAL NZ</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/nzstFeed' onClick={() => selectCategory('Science and Technology')}><H3>SCIENCE AND TECHNOLOGY</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/nzstFeed' onClick={() => selectCategory('Science and Technology')}><NavCategories>SCIENCE AND TECHNOLOGY</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/qcFeed' onClick={() => selectCategory('Quantum Computing')}><H3>QUANTUM COMPUTING</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/qcFeed' onClick={() => selectCategory('Quantum Computing')}><NavCategories>QUANTUM COMPUTING</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='/qpFeed' onClick={() => selectCategory('Quantum Physics')}><H3>QUANTUM PHYSICS</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='/qpFeed' onClick={() => selectCategory('Quantum Physics')}><NavCategories>QUANTUM PHYSICS</NavCategories></Link>
         </NavLi>
         <NavLi>
-          <Link style={{ textDecoration: 'none' }} to='spaceFeed' onClick={() => selectCategory('Space')}><H3>SPACE</H3></Link>
+          <Link style={{ textDecoration: 'none' }} to='spaceFeed' onClick={() => selectCategory('Space')}><NavCategories>SPACE</NavCategories></Link>
         </NavLi>
       </NavUl>
     </nav>
