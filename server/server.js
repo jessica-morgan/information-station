6a7b79e4e486b59c74a269be4f80f56513c3b889
jess morgan <jess-morgan1@outlook.com>
2019-09-27 15:32:10 +1200

    cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const server = express()
 
+server.use(cors({ origin: 'http://localhost:8080' }))
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
