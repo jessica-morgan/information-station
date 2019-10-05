92b1ab234b5f153695f746ba5cc2f0750c785d46
jess morgan <jess-morgan1@outlook.com>
2019-10-05 16:36:49 +1300

    cors

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 const server = express()
 
 server.use(express.json())
-server.use(cors({ origin: 'http://localhost:8080' }))
+server.use(cors())
 server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/api/v1/', routes)
