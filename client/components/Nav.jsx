ff9ac1cb62bd89be79c2f6f63ff09f8b0c4f80c1
jess morgan <jess-morgan1@outlook.com>
2019-11-15 18:36:42 +1300

    make nav bar responsive

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'reactn'
 import { selectCategory } from '../utils'
 import { Link } from 'react-router-dom'
-import { NavCategories, NavContainer, NavCategoryContainer } from '../styles'
+import { NavContainer, NavCategoryContainer } from '../styles'
 import CurrentCategoryTitle from './CurrentCategoryTitle'
 
 const Nav = () => {
@@ -12,28 +12,28 @@ const Nav = () => {
         <CurrentCategoryTitle/>
       </NavCategoryContainer>
       <NavCategoryContainer row1>
-        <Link style={{ textDecoration: 'none' }} to='/phFeed' onClick={() => selectCategory('Programmer Humor')}><NavCategories>PROGRAMMER HUMOR</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='/phFeed' onClick={() => selectCategory('Programmer Humor')}><NavCategoryContainer title="true">PROGRAMMER HUMOR</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row2>
-        <Link style={{ textDecoration: 'none' }} to='/rjsFeed' onClick={() => selectCategory('Reactjs')}><NavCategories>REACTJS</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='/rjsFeed' onClick={() => selectCategory('Reactjs')}><NavCategoryContainer title="true">REACTJS</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row3>
-        <Link style={{ textDecoration: 'none' }} to='/cgpFeed' onClick={() => selectCategory('Cool Github Projects')}><NavCategories>COOL GITHUB PROJECTS</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='/cgpFeed' onClick={() => selectCategory('Cool Github Projects')}><NavCategoryContainer title="true">COOL GITHUB PROJECTS</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row4>
-        <Link style={{ textDecoration: 'none' }} to='jsFeed' onClick={() => selectCategory('Javascript')}><NavCategories>JAVASCRIPT</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='jsFeed' onClick={() => selectCategory('Javascript')}><NavCategoryContainer title="true">JAVASCRIPT</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row5>
-        <Link style={{ textDecoration: 'none' }} to='/nzgFeed' onClick={() => selectCategory('General NZ')}><NavCategories>GENERAL NZ</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='/nzgFeed' onClick={() => selectCategory('General NZ')}><NavCategoryContainer title="true">GENERAL NZ</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row6>
-        <Link style={{ textDecoration: 'none' }} to='/qcFeed' onClick={() => selectCategory('Quantum Computing')}><NavCategories>QUANTUM COMPUTING</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='/qcFeed' onClick={() => selectCategory('Quantum Computing')}><NavCategoryContainer title="true">QUANTUM COMPUTING</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row7>
-        <Link style={{ textDecoration: 'none' }} to='/qpFeed' onClick={() => selectCategory('Quantum Physics')}><NavCategories>QUANTUM PHYSICS</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='/qpFeed' onClick={() => selectCategory('Quantum Physics')}><NavCategoryContainer title="true">QUANTUM PHYSICS</NavCategoryContainer ></Link>
       </NavCategoryContainer>
       <NavCategoryContainer row8>
-        <Link style={{ textDecoration: 'none' }} to='spaceFeed' onClick={() => selectCategory('Space')}><NavCategories>SPACE</NavCategories></Link>
+        <Link style={{ textDecoration: 'none' }} to='spaceFeed' onClick={() => selectCategory('Space')}><NavCategoryContainer title="true">SPACE</NavCategoryContainer ></Link>
       </NavCategoryContainer>
     </NavContainer>
     </>
